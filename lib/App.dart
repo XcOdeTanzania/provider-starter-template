9cc7e9282650a8132fa142d0406b5e44b8931374
admin <kalrobbynson@gmail.com>
2020-08-15 21:02:50 +0300

    constants for route pages

--- lib/App.dart
+++ lib/App.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider_starter_template/constants/routes.dart';
 import 'package:provider_starter_template/views/pages/home_page.dart';
+import 'package:provider_starter_template/views/pages/login_page.dart';
 
 class App extends StatelessWidget {
   // This widget is the root of your application.
@@ -8,18 +10,13 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        visualDensity: VisualDensity.comfortable,
       ),
-      home: HomePage(),
+      home: LoginPage(),
+      routes: {
+        homePageRoute: (context) => HomePage(),
+      },
     );
   }
 }
