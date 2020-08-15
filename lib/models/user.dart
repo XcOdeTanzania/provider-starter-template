fff0fc69b11eb194968ce06dd2c97ff3b53ae8b7
admin <kalrobbynson@gmail.com>
2020-08-15 20:39:53 +0300

    user model added

---
+++ lib/models/user.dart
@@ -0,0 +1,9 @@
+///model class for [User] blueprint
+import 'package:flutter/foundation.dart';
+
+class User {
+  ///a string variable to hold [username]
+  final String username;
+
+  User({@required this.username});
+}
